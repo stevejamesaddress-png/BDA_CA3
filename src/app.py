5249ab33b9a7e4afcc8556295516b2bed9880166
AI Kids <interplanatary@outlook.de>
2026-05-24 15:01:37 +0200

    Testing again

--- src/app.py
+++ src/app.py
@@ -52,7 +52,7 @@ def home():
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    logger.info("Predicting")
+    logger.info("Predicting test")
     try:
         #get the querry values from HTTP request
         sepal_length = float(request.args.get('sepal_length'))
